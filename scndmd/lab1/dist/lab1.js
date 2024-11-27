fafeb11a1d2eb07d09da930128e0bad110d9b673
catchtimings <chindina.63@gmail.com>
2024-11-27 17:37:56 +0400

    add arrow function and output in console

--- scndmd/lab1/dist/lab1.js
+++ scndmd/lab1/dist/lab1.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+
+const result = multiply(5, 10);
+
+const jsonStringf = JSON.stringify(result)
+
+console.log(result);
+console.log(jsonStringf);
